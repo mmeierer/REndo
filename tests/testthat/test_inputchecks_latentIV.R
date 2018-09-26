02cb4ff8934081664be4b0925b60abdf92298b5a
Patrik <patrik.schilter@gmail.com>
2018-09-26 17:40:52 +0200

    latentIV: Cleanup tests
    move formula and data tests from separate file to main inputcheck tests

--- tests/testthat/test_inputchecks_latentIV.R
+++ tests/testthat/test_inputchecks_latentIV.R
@@ -5,10 +5,109 @@ data("dataLatentIV")
 
 # formula --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 context("latentIV - Parameter formula")
-test.formula.latentIV(dataLatentIV=dataLatentIV)
+
+test_that("Fail if no formula object is passed",  {
+  expect_error(latentIV(formula = data.frame(1:3), data = dataLatentIV), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = NULL,            data = dataLatentIV), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = NA,              data = dataLatentIV), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula =   ,              data = dataLatentIV), regexp = "The above errors were encountered!")
+})
+
+test_that("Fail if bad 1st RHS", {
+  # Fail if > 1 regressor in RHS
+  expect_error(latentIV(formula = y ~ I + P ,       data = cbind(I=1, dataLatentIV)),              regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y ~ X1 + X2 + P , data = cbind(X1=1.23, X2=2.34, dataLatentIV)), regexp = "The above errors were encountered!")
+})
+
+test_that("Fail if bad 2nd RHS", {
+  # Fail if ANY 2nd RHS
+  expect_error(latentIV(formula = y ~ P | P, data = dataLatentIV), regexp = "The above errors were encountered!")
+})
+
+test_that("Fail if bad LHS", {
+  # Fail for missing LHS
+  expect_error(latentIV(formula = ~ P, data = dataLatentIV), regexp = "The above errors were encountered!")
+  # Fail for > 1 LHS
+  expect_error(latentIV(formula = y1 + y2      ~ P, data = dataLatentIV), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y1 + y2 + y3 ~ P, data = dataLatentIV), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y1 | y2      ~ P, data = dataLatentIV), regexp = "The above errors were encountered!") # multipart LHS
+  # Fail for LHS in RHS and vice-versa
+  expect_error(latentIV(formula = P ~ P, data = dataLatentIV), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y ~ y, data = dataLatentIV), regexp = "The above errors were encountered!")
+  # Fail for dot in LHS as not more than one LHS allowed
+  # expect_error(latentIV(formula = . ~ P, data = dataLatentIV), regexp = "The above errors were encountered!")
+})
+
+test_that("Fail if formula variables are not in data", {
+  # Fail if any regressors not in data (RHS1, RHS2, LHS1)
+  expect_error(latentIV(formula = y ~ P, data = data.frame(y=1:10, X1=1:10, X2=1:10)), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y ~ P, data = data.frame(X1=1:10,X2=1:10, P=1:10)),  regexp = "The above errors were encountered!")
+})
+
+test_that("Fail if formula contains dot (.)", {
+  # Fail if dot (.) is in formula in any part
+  expect_error(latentIV(formula = . ~ P, data = dataLatentIV), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y ~ ., data = dataLatentIV), regexp = "The above errors were encountered!")
+})
+
+# **** FIX LATENTIV
+# test_that("Transformations can be handled and are correct", {
+#   correct.res <- latentIV(formula = y ~ P, data = dataLatentIV)
+#   # Can handle transformations in LHS
+#   data.altered   <- dataLatentIV
+#   data.altered$y <- data.altered$y*2.5
+#   res.trans.lhs <- latentIV(formula = I(y/2.5) ~ P, data = data.altered)
+#   expect_equal(coef(res.trans.lhs), coef(correct.res), check.attributes=F)
+#   # expect_equal(coef(summary(res.trans.lhs)), coef(summary(correct.res))) # will vary greatly because of limited bootstrappings
+#
+#   # Can handle transformations in RHS1
+#   data.altered    <- dataLatentIV
+#   data.altered$X1 <- data.altered$P+111
+#   res.trans.rhs1  <- latentIV(formula = y ~ I(P-111), data = data.altered)
+#   expect_equal(coef(res.trans.rhs1), coef(correct.res), check.attributes=F)
+#   # expect_equal(coef(summary(res.trans.rhs1)), coef(summary(correct.res))) # will vary greatly because of limited bootstrappings
+# })
+
+test_that("Fail if formula variables are not in data", {
+  # Fail if any regressors not in data (RHS1, RHS2, LHS1)
+  expect_error(latentIV(formula= y ~ P ,data=data.frame(y=1:10)), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula= y ~ P ,data=data.frame(P=1:10)), regexp = "The above errors were encountered!")
+})
+
 
 context("latentIV - Parameter data")
-test.data.latentIV(dataLatentIV=dataLatentIV)
+test_that("Fail if not data.frame", {
+  expect_error(latentIV(formula = y ~ P, data =     ),                regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y ~ P, data = NULL),                regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y ~ P, data = NA_integer_),         regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y ~ P, data = c(y=1:10,   P=1:10)), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y ~ P, data = list(y=1:10,P=1:10)), regexp = "The above errors were encountered!")
+})
+
+test_that("Fail if no rows or cols",{
+  expect_error(latentIV(formula = y ~ P, data = data.frame()), regexp = "The above errors were encountered!") # no cols
+  expect_error(latentIV(formula = y ~ P, data = data.frame(y=integer(), P=integer())), regexp = "The above errors were encountered!")
+})
+
+test_that("Fail if wrong data type in any of the formula parts", {
+  # Only allow numericals in all relevant columns
+  # Factor
+  expect_error(latentIV(formula = y ~ P, data = data.frame(y=factor(1:10), P=1:10)), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y ~ P, data = data.frame(y=1:10, P=factor(1:10)), regexp = "The above errors were encountered!"))
+  # Characters
+  expect_error(latentIV(formula = y ~ P, data = data.frame(y=as.character(1:10), P=1:10, stringsAsFactors=F)), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y ~ P, data = data.frame(y=1:10, P=as.character(1:10), stringsAsFactors=F)), regexp = "The above errors were encountered!")
+  # Logicals (as indicate dichotomous variable (=factor))
+  expect_error(latentIV(formula = y ~ P, data = data.frame(y=as.logical(0:9), P=1:10)), regexp = "The above errors were encountered!")
+  expect_error(latentIV(formula = y ~ P, data = data.frame(y=1:10, P=as.logical(0:9)), regexp = "The above errors were encountered!"))
+})
+
+test_that("Allow wrong data type in irrelevant columns", {
+  # Allow wrong data types in unused columns
+  expect_silent(latentIV(formula = y ~ P, start.params = c("(Intercept)"=2.9, P = -0.852), verbose = FALSE,
+                         data = cbind(dataLatentIV,
+                                      unused1=as.logical(0:9), unused2=as.character(1:10),unused3=as.factor(1:10), stringsAsFactors = F)))
+})
 
 
 # start.params ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
