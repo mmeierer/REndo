f26b57eb31b4caa50a5a7812f2d7074ad0d97446
Patrik <patrik.schilter@gmail.com>
2018-11-01 14:52:09 +0100

    documentation: Fix issues

--- R/REndo-package.R
+++ R/REndo-package.R
@@ -9,16 +9,16 @@
 #' These are statistical techniques to address the endogeneity problem where no external instrumental variables are needed.
 #'
 #' The main functions to estimate models are:
-#' \begin{itemize}
-#' \item \code{latentIV()} - the latent instrumental variables method of Ebbes et al. (2005)
-#' \item \code{copulaCorrection()} - copula correction method proposed by Paek and Gupta (2012)
-#' \item \code{hetErrorsIV()} - heteroskedastic errors approach proposed by Lewbel(2012)
-#' \item \code{higherMomentsIV()} - higher moments method proposed by Lewbel (1997)
-#' \item \code{multilevelIV()} - multilevel GMM method proposed by Kim and Frees (2007)
-#' \end{itemize}
-#' 
-#' \textbf{Differences between current (2.0) and previous version of REndo}\\
-#' 
+#' \itemize{
+#' \item{\code{latentIV()}}{the latent instrumental variables method of Ebbes et al. (2005)}
+#' \item{\code{copulaCorrection()}}{copula correction method proposed by Paek and Gupta (2012)}
+#' \item{\code{hetErrorsIV()}}{heteroskedastic errors approach proposed by Lewbel(2012)}
+#' \item{\code{higherMomentsIV()}}{higher moments method proposed by Lewbel (1997)}
+#' \item{\code{multilevelIV()}}{multilevel GMM method proposed by Kim and Frees (2007)}
+#' }
+#'
+#' \strong{Differences between current (2.0.0) and previous version of REndo}
+#'
 #' Note that with version 2.0.0 sweeping changes were which greatly improve functionality but break backwards compatibility.
 #' Various bugs were fixed, performance improved, handling of S3 objects and methods across the package was harmonized,
 #' and a set of argument checks has been added. Starting with REndo 2.0, all functions support the use
--- R/data.R
+++ R/data.R
@@ -25,7 +25,7 @@
 #'  \code{P1} and \code{P2}, having a T-distribution with 3 degrees of freedom.
 #'  An intercept and a dependent variable, \code{y}, are also included.
 #'  The true parameter values for the intercept and the exogenous regressors' coefficients are: \code{b0 = 2}, \code{b1 = 1.5},
-#'  \code{b2 = -3}. The coefficient of the endogenous regressor \code{P1} is equal to \code{a1 = -1} and 
+#'  \code{b2 = -3}. The coefficient of the endogenous regressor \code{P1} is equal to \code{a1 = -1} and
 #'  of \code{P2} is equal to \code{a2 = 0.8}.
 #' @name dataCopCont2
 #' @usage dataCopCont2
@@ -36,7 +36,7 @@
 #' \item{\code{X2}}{a numeric vector, normally distributed and exogenous.}
 #' \item{\code{P1}}{a numeric vector, continuous and endogenous having T-distribution with 3 degrees of freedom.}
 #' \item{\code{P2}}{a numeric vector, continuous and endogenous having T-distribution with 3 degrees of freedom.}
-}
+#' }
 #' @docType data
 #' @author Raluca Gui \email{raluca.gui@@business.uzh.ch}
 "dataCopCont2"
@@ -53,11 +53,12 @@
 #' @usage dataCopDisCont
 #' @format A data frame with 2500 observations on 6 variables:
 #' \describe{
-#' \item{\code{y}}{a numeric vector representing the dependent variable}.
+#' \item{\code{y}}{a numeric vector representing the dependent variable.}
 #' \item{\code{X1}}{a numeric vector, normally distributed and exogenous.}
 #' \item{\code{X2}}{a numeric vector, normally distributed and exogenous.}
 #' \item{\code{P1}}{a numeric vector, continuous and endogenous having Poisson distribution with parameter lambda equal to 3.}
-#' \item{\code{P2}}{a numeric vector, continuous and endogenous having T-distribution with 3 degrees of freedom.}}
+#' \item{\code{P2}}{a numeric vector, continuous and endogenous having T-distribution with 3 degrees of freedom.}
+#' }
 #' @docType data
 #' @author Raluca Gui \email{raluca.gui@@business.uzh.ch}
 "dataCopDisCont"
@@ -88,7 +89,7 @@
 #'  \code{P1} and \code{P2}.
 #'  An intercept and a dependent variable, \code{y}, are also included.
 #'  The true parameter values for the coefficients of the intercept and the exogenous variables are: \code{b0 = 2}, \code{b1 = 1.5},
-#'  \code{b2 = -3}. The true parameter values for the coefficients of the endogenous regressors are \code{a1 = -1} for \code{P1} and 
+#'  \code{b2 = -3}. The true parameter values for the coefficients of the endogenous regressors are \code{a1 = -1} for \code{P1} and
 #'  \code{a2 = 0.8} for \code{P2}.
 #' @name dataCopDis2
 #' @usage dataCopDis2
@@ -99,6 +100,7 @@
 #' \item{\code{X2}}{a numeric vector, normally distributed and exogenous.}
 #' \item{\code{P1}}{a numeric vector, having a Poisson distribution with parameter lambda equal to 3, and endogenous.}
 #' \item{\code{P2}}{a numeric vector, having a Poisson distribution with parameter lambda equal to 3, and endogenous.}
+#' }
 #' @docType data
 #' @author Raluca Gui \email{raluca.gui@@business.uzh.ch}
 "dataCopDis2"
