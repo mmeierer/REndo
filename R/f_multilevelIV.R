28196202af987a6d74a84bdab65fbb2512c69f54
Patrik <patrik.schilter@gmail.com>
2018-11-01 14:34:52 +0100

    multilevel: Add details to docu

--- R/f_multilevelIV.R
+++ R/f_multilevelIV.R
@@ -9,6 +9,8 @@
 #' Estimates multilevel models (max. 3 levels) employing the GMM approach presented in Kim and Frees (2007).
 #' One of the important features is that, using the hierarchical structure of the data, no external instrumental
 #' variables are needed, unlike traditional instrumental variable techniques
+#' @details
+#' Method will be added to the package in the next major release.
 #' @references   Kim, Jee-Seon and Frees, Edward W. (2007). "Multilevel Modeling with Correlated Effects". Psychometrika, 72(4), 505-533.
 #' @export
 multilevelIV <- function(formula, data, verbose=TRUE){
