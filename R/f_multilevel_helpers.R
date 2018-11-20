e1bb020537bb8c2f9857009e4505b4e448e9f866
Patrik <patrik.schilter@gmail.com>
2018-11-20 23:51:13 +0100

    rename buildmatrices to helpers

--- R/f_multilevel_helpers.R
+++ R/f_multilevel_helpers.R
@@ -8,9 +8,22 @@ multilevel_splittomatrix <- function(dt, name.group, name.by){
   return(l.groups)
 }
 
-
+#' @importFrom Matrix Matrix
 multilevel_colstomatrix <- function(dt, name.cols){
   .SD <- NULL
   return(Matrix::Matrix(as.matrix(dt[, .SD, .SDcols = c(name.cols, "rn")],
                                   rownames = "rn"),  sparse = TRUE))
 }
+
+
+
+#' #' Formula:
+#' #'   p510: P(H) = H(H'H)^(-1)H'
+#' #' @importFrom corpcor pseudoinverse
+#' multilevel_projectionP <- function(H){
+#'   # crossprod:  t(x) %*% y
+#'   # tcrossprod: x %*% t(y)
+#'   # Matrix::crossprod returns a dsCMatrix (ie knows its symmetric)
+#'   # return(H %*% Matrix::tcrossprod( Matrix::solve(Matrix::crossprod(H), sparse=TRUE), H))
+#'   return(H %*% Matrix::tcrossprod( corpcor::pseudoinverse(Matrix::crossprod(H), sparse=TRUE), H))
+#' }
