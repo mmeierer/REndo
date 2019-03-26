249f5452d1566c65340cdc6305966588edc30c30
Patrik <patrik.schilter@gmail.com>
2019-03-26 17:19:33 +0100

    copulaCorrection: s3 tests
    remove obsolete discrete only confint checks. replace with general boots s3 methods checks

--- tests/testthat/test_s3methods_copualcorrection.R
+++ tests/testthat/test_s3methods_copualcorrection.R
@@ -8,117 +8,6 @@ data("dataCopDis2") # only use 2 case version
 data("dataCopCont")
 data("dataCopCont2")
 data("dataCopDisCont")
-#
-# Discrete case --------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# Standard S3 methods checks
-context("S3methods - copulaCorrection - 1 discrete")
-
-# d.input.form <- y ~ X1 + X2 + P1+P2|discrete(P1, P2)
-# expect_warning(res.d <- copulaCorrection(formula = d.input.form, data = dataCopDis2, verbose=FALSE, num.boots=2),
-#                regexp = "It is recommended to run 1000 or more bootstraps.", all = TRUE)
-#
-# test_that("Discrete confint placeholder", {
-#   # Put in separate test_that to be able to skip it on cran
-#   skip_on_cran()
-#
-#   #include placeholder test as otherwise the whole block will be skipped because it is regarded as empty
-#   expect_true(TRUE)
-#
-#   test.s3methods.rendoboots(res.model =res.d, input.form=d.input.form, function.std.data=dataCopDis2,
-#                             full.coefs=c("(Intercept)", "X1", "X2", "P1", "P2", "PStar.P1", "PStar.P2"))
-# })
-#
-#
-
-# # Discrete case confint --------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# context("S3methods - copulaCorrection - discrete confint")
-# # Do not use .S3.helper.confint because also num.sumulations can be supplied
-#
-# # Get results to work with
-# expect_warning(res.dis.only <- copulaCorrection(formula=y~X1+X2+P1+P2|discrete(P1, P2),
-#                                                data=dataCopDis2, verbose = FALSE, num.boots = 2),
-#                regexp = "It is recommended to run 1000 or more bootstraps.", all=TRUE)
-#
-# test_that("Confint works with different alphas", {
-#   expect_silent(ci.99 <- confint(res.dis.only, level = 0.99))
-#   expect_silent(ci.95 <- confint(res.dis.only, level = 0.95))
-#   expect_silent(ci.90 <- confint(res.dis.only, level = 0.90))
-#   expect_silent(ci.70 <- confint(res.dis.only, level = 0.70))
-#
-#   # Level works and provides different values
-#   expect_false(isTRUE(all.equal(ci.99,ci.95,check.attributes=FALSE)))
-#   expect_false(isTRUE(all.equal(ci.95,ci.90,check.attributes=FALSE)))
-#   expect_false(isTRUE(all.equal(ci.90,ci.70,check.attributes=FALSE)))
-#
-#   # Rightly named, all same
-#   expect_equal(rownames(ci.99), names(coef(res.dis.only)))
-#   expect_equal(rownames(ci.99), rownames(ci.95))
-#   expect_equal(rownames(ci.95), rownames(ci.90))
-#   expect_equal(rownames(ci.90), rownames(ci.70))
-# })
-#
-# test_that("Confint works with character param", {
-#   # Single
-#   for(p in names(coef(res.dis.only)))
-#     expect_equal(rownames(confint(res.dis.only,num.simulations=10, parm = p)), expected = p)
-#   # Multiple
-#   p <- c("X1","X2","P1")
-#   expect_equal(rownames(confint(res.dis.only,num.simulations=10, parm = p)), expected = p)
-#   p <- c("X2","P1","P2")
-#   expect_equal(rownames(confint(res.dis.only,num.simulations=10, parm = p)), expected = p)
-#   # All - excplicitely
-#   p <- c("(Intercept)","X1","X2","P1","P2", "PStar.P1", "PStar.P2")
-#   expect_equal(rownames(confint(res.dis.only,num.simulations=10, parm = p)), expected = p)
-#   # All - implicitely (ie none given)
-#   expect_equal(rownames(confint(res.dis.only)), expected = p)
-# })
-#
-# test_that("Confint works with integer param", {
-#   p <- names(coef(res.dis.only))
-#   # Single
-#   expect_equal(rownames(confint(res.dis.only,num.simulations=10, parm = 3)), expected = p[3])
-#   expect_equal(rownames(confint(res.dis.only,num.simulations=10, parm = 5)), expected = p[5])
-#   # Sequence
-#   expect_equal(rownames(confint(res.dis.only,num.simulations=10, parm = 1:3)), expected = p[1:3])
-#   expect_equal(rownames(confint(res.dis.only,num.simulations=10, parm =c(2,4,6))), expected = p[c(2,4,6)])
-#   # All - excplicitely
-#   expect_equal(rownames(confint(res.dis.only,num.simulations=10, parm = seq(length(p)))), expected = p)
-#   # All - implicitely (ie none given)
-#   expect_equal(rownames(confint(res.dis.only)), expected = p)
-#
-#   # Minus removes
-#   expect_equal(rownames(confint(res.dis.only,num.simulations=10, parm = -2)), expected = p[-2])
-#   expect_equal(rownames(confint(res.dis.only,num.simulations=10, parm = -c(2,4))), expected = p[-c(2,4)])
-#   # Remove all
-#   expect_null(rownames(confint(res.dis.only,num.simulations=10, parm = -seq(length(p)))))
-# })
-#
-# # same behavior as lm
-# test_that("confint NA if unknown parm", {
-#   # Unknown character
-#   expect_true(all(is.na( confint(res.dis.only,num.simulations=10, parm = "abc") )))
-#   expect_true(all(is.na( confint(res.dis.only,num.simulations=10, parm = c("abc", "zcgd")) )))
-#   # Wrong indices
-#   expect_true(all(is.na( confint(res.dis.only,num.simulations=10, parm = 50:100))))
-#   expect_true(all(is.na( confint(res.dis.only,num.simulations=10, parm = 99) )))
-#   # Part of it are known
-#   expect_true(all(names(coef(res.dis.only)) %in%
-#                 rownames(confint(res.dis.only,num.simulations=10, parm = 1:100))))
-#   expect_true(!all(is.na( confint(res.dis.only,num.simulations=10, parm = 1:100))))
-# })
-#
-# test_that("num.simulations has integer default value", {
-#   expect_is(eval(formals(REndo:::confint.rendo.boots)[["num.simulations"]]), "integer")
-# })
-#
-#
-# test_that("Not discrete only returns normal lm confint", {
-#   expect_warning(res.dis.cont <- copulaCorrection(formula=y~X1+X2+P1+P2|discrete(P1)+continuous(P2),
-#                                                  data=dataCopDisCont, verbose = FALSE, num.boots = 2),
-#                  regexp = "It is recommended to run 1000 or more bootstraps.", all=TRUE)
-#   expect_identical(confint(res.dis.cont), confint.lm(res.dis.cont))
-# })
-
 
 # C1 optim LL case ----------------------------------------------------------------------------------------------------
 context("S3methods - copulaCorrection - 1 continuous")
@@ -186,6 +75,29 @@ test_that("Case 2 summary has additional specific structure", {
   expect_is(res.sum$names.vars.discrete, "character")
 
 })
+
+# C2 Discrete case --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+context("S3methods - copulaCorrection - 2 discrete")
+
+d.input.form <- y ~ X1 + X2 + P1+P2|discrete(P1, P2)
+expect_warning(res.d <- copulaCorrection(formula = d.input.form, data = dataCopDis2, verbose=FALSE, num.boots=100),
+               regexp = "It is recommended to run 1000 or more bootstraps.", all = TRUE)
+
+test.s3methods.rendoboots(res.model =res.d, input.form=d.input.form, function.std.data=dataCopDis2,
+                            full.coefs=c("(Intercept)", "X1", "X2", "P1", "P2", "PStar.P1", "PStar.P2"))
+
+test_that("Case 2 summary has additional specific structure", {
+  expect_silent(res.sum <- summary(res.d))
+  expect_s3_class(res.sum, "summary.rendo.copula.c2")
+  expect_true(is.list(res.sum))
+  expect_true(all(c("names.vars.continuous", "names.vars.discrete") %in%
+                    names(res.sum)))
+  expect_is(res.sum$names.vars.continuous, "character")
+  expect_is(res.sum$names.vars.discrete, "character")
+
+})
+
 
 # C2 Mixed case -------------------------------------------------------------------------------------------------------------
 context("S3methods - copulaCorrection - 1 continuous, 1 discrete")
