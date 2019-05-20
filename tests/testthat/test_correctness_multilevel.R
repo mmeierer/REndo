4c41dbe013658743e2c22a7c5b2079099639a1c3
Patrik <patrik.schilter@gmail.com>
2019-05-20 12:13:48 +0200

    multilevel: more tests
    uncomment

--- tests/testthat/test_correctness_multilevel.R
+++ tests/testthat/test_correctness_multilevel.R
@@ -191,33 +191,36 @@ test_that("REF is same as lmer()", {
 
 })
 
-# test_that("Retrieve generated data params", {
-#   expect_silent(res.ml3 <- multilevelIV(formula = y ~ X11 + X12 + X13 + X14 + X15 + X21 + X22 + X23 + X24 +
-#                                           X31 + X32 + X33 + (1|CID)+(1| SID) | endo(X15),
-#                                         data = dataMultilevelIV, verbose = FALSE))
-#
-#   correct.coefs <- c("(Intercept)"=64,
-#                      X11 = 3, X12=9, X13=-2, X14 = 2,
-#                      X15 = -1, #endo
-#                      X21 = -1.5, X22 = -4, X23 = -3, X24 = 6,
-#                      X31 = 0.5, X32 = 0.1, X33 = -0.5)
-#
-#   expect_equal(coef(res.ml3), correct.coefs, tolerance = 0.1)
-# })
+test_that("Retrieve generated data params", {
+  skip_on_cran()
+  expect_silent(res.ml3 <- multilevelIV(formula = y ~ X11 + X12 + X13 + X14 + X15 + X21 + X22 + X23 + X24 +
+                                          X31 + X32 + X33 + (1|CID)+(1| SID) | endo(X15),
+                                        data = dataMultilevelIV, verbose = FALSE))
 
-# test_that("Reproduce results by Kim and Frees 2007", {
-#   kf.formula <- TLI ~ GRADE_3 +  RETAINED  + SWITCHSC + S_FREELU +
-#                   FEMALE + BLACK + HISPANIC + OTHER+ C_COHORT+
-#                   T_EXPERI + CLASS_SI+ P_MINORI + (1 + GRADE_3|NEWCHILD) | endo(CLASS_SI)
-#
-#   # Is in tests/testthat folder
-#   df.data.kf <- read.csv("dallas2485.csv", header=TRUE)
-#
-#   expect_silent(res.kf <- multilevelIV(formula = kf.formula, data = df.data.kf, verbose = FALSE))
-#
-#   correct.coefs <- cbind(REF = c(3.375, 9.205, -0.365, -0.227, -1.234, -4.745, -3.608, 6.526, 1.497, -0.116, 0.157, 0.069))
-#
-#   # Compare coefs
-#   expect_equal(coef(res.kf) )
-#
-# })
+  correct.coefs <- c("(Intercept)"=64,
+                     X11 = 3, X12=9, X13=-2, X14 = 2,
+                     X15 = -1, #endo
+                     X21 = -1.5, X22 = -4, X23 = -3, X24 = 6,
+                     X31 = 0.5, X32 = 0.1, X33 = -0.5)
+
+  expect_equal(coef(res.ml3)[, "REF"], correct.coefs, tolerance = 0.1)
+})
+
+test_that("Reproduce results by Kim and Frees 2007", {
+  skip_on_cran()
+  kf.formula <- TLI ~ GRADE_3 +  RETAINED  + SWITCHSC + S_FREELU +
+                  FEMALE + BLACK + HISPANIC + OTHER+ C_COHORT+
+                  T_EXPERI + CLASS_SI+ P_MINORI + (1 + GRADE_3|NEWCHILD) | endo(CLASS_SI)
+
+  # Is in tests/testthat folder
+  df.data.kf <- read.csv("dallas2485.csv", header=TRUE)
+
+  expect_silent(res.kf <- multilevelIV(formula = kf.formula, data = df.data.kf, verbose = FALSE))
+
+  correct.coefs <- cbind(REF = c("(Intercept)"=69.78, GRADE_3=3.375, RETAINED=9.205, SWITCHSC=-0.365, S_FREELU=-0.227, FEMALE=-1.234, BLACK=-4.745,
+                                 HISPANIC=-3.608, OTHER=6.526, C_COHORT=1.497, T_EXPERI=-0.116, CLASS_SI=0.157, P_MINORI=0.069))
+
+  # Compare coefs
+  expect_equal(coef(res.kf)[, "REF"], correct.coefs[, "REF"], tolerance=0.1)
+
+})
