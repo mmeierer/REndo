2d9aff8f1accbb681ab04f9c3d30d887ba272d2e
Patrik <patrik.schilter@gmail.com>
2020-02-18 23:07:59 +0100

    cleanup: Remove namespace access
    using namespace Rcpp

--- src/f_copulacorrection_LL_rcpp.cpp
+++ src/f_copulacorrection_LL_rcpp.cpp
@@ -2,17 +2,20 @@
 
 // [[Rcpp::depends(RcppEigen)]]
 
+// To increase readability for R users, do not put Rcpp:: before every command
+using namespace Rcpp;
+
 // [[Rcpp::export]]
-double copulaCorrection_LL_rcpp(const Rcpp::NumericVector& params,
-                                const Rcpp::NumericVector& vec_y,
-                                const Rcpp::NumericMatrix& m_data_exo_endo,
-                                const Rcpp::NumericVector& vec_data_endo_pstar,
-                                const Rcpp::NumericVector& param_pos_data,
-                                const double param_pos_sigma,
-                                const double param_pos_rho){
+double copulaCorrection_LL_rcpp(const NumericVector& params,
+                                const NumericVector& vec_y,
+                                const NumericMatrix& m_data_exo_endo,
+                                const NumericVector& vec_data_endo_pstar,
+                                const NumericVector& param_pos_data,
+                                const double& param_pos_sigma,
+                                const double& param_pos_rho){
 
   // # Extract params from optimx inputs --------------------------------------------------------
-  Rcpp::NumericVector params_endo_exo = params[param_pos_data - 1];
+  NumericVector params_endo_exo       = params[param_pos_data - 1];
   double sigma                        = params[param_pos_sigma - 1];
   double rho                          = params[param_pos_rho - 1];
 
@@ -30,7 +33,7 @@ double copulaCorrection_LL_rcpp(const Rcpp::NumericVector& params,
 
   // # epsilon, incl. endo regressor ------------------------------------------------------------
   // # Reorder params to fit matrix col order
-  Rcpp::CharacterVector m_colnames = Rcpp::colnames(m_data_exo_endo);
+  CharacterVector m_colnames = colnames(m_data_exo_endo);
   params_endo_exo = params_endo_exo[m_colnames];
 
   // Short excursion to RcppEigen: Matrix multiplication
@@ -38,28 +41,28 @@ double copulaCorrection_LL_rcpp(const Rcpp::NumericVector& params,
   const Eigen::VectorXd B = Rcpp::as<Eigen::VectorXd>(params_endo_exo);
   Eigen::MatrixXd C = A * B;
   // Back to regular Rcpp again
-  Rcpp::NumericVector matMultRes = Rcpp::wrap(A*B);
+  NumericVector matMultRes = wrap(A*B);
 
-  Rcpp::NumericVector eps_1 = vec_y - matMultRes;
+  NumericVector eps_1 = vec_y - matMultRes;
 
   // # PPnorm -----------------------------------------------------------------------------------
   // # residulas - epsilon should be normally distributed
-  Rcpp::NumericVector ppnorm   = Rcpp::pnorm(eps_1, 0.0, sigma);
+  NumericVector ppnorm         = pnorm(eps_1, 0.0, sigma);
   ppnorm[ppnorm >= 0.999998]   = 0.999998;
   ppnorm[ppnorm <= 0.0001]     = 0.0001;
 
   // # epsilon star -----------------------------------------------------------------------------
   // eps.star <- stats::qnorm(ppnorm)
-  Rcpp::NumericVector eps_star = Rcpp::qnorm(ppnorm);
+  NumericVector eps_star = qnorm(ppnorm);
 
   // # l.eps ------------------------------------------------------------------------------------
   // l.eps <- sum(dnorm(eps.1,mean=0,sd=sigma, log = TRUE))
-  double l_eps = Rcpp::sum(Rcpp::dnorm(eps_1, 0.0, sigma, true));
+  double l_eps = sum(dnorm(eps_1, 0.0, sigma, true));
 
   // # s
   // s <- sum((vec.data.endo.pstar^2 + eps.star^2)/(2*(1-rho^2)) -
   //   (rho*vec.data.endo.pstar*eps.star)/(1-rho^2))
-  double s = Rcpp::sum((Rcpp::pow(vec_data_endo_pstar,2.0) + Rcpp::pow(eps_star,2.0)) /
+  double s = sum((pow(vec_data_endo_pstar,2.0) + pow(eps_star,2.0)) /
                        (2*(1-std::pow(rho,2.0))) -
              (rho*vec_data_endo_pstar*eps_star)/(1-std::pow(rho,2.0)));
   // # mm
