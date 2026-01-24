cce7869d310d19cbaa4bb297ad0649c7fe207ee5
KLewCW <kimberlylew12@gmail.com>
2026-01-24 15:49:49 +0100

    Changes to old file 'f_S3_copulaIMA_bootstrap.R'
    1) Renamed the file 'f_S3_copulaIMA_bootstrap.R' to f_new_rendocopulaima.R'.
    
    2) Removed '@importFrom formula as.formula'.
    
    3) Removed the 'without bootstrap' method. As discusses for now, we force users to only use with bootstrap.

--- R/f_new_rendocopulaima.R
+++ R/f_new_rendocopulaima.R
@@ -1,20 +1,4 @@
-#' @importFrom Formula as.Formula
 #'
-
-#without bootstrap
-build_rendo_base_ima <- function(call, F.formula, mf, estimated) {
-  .new_rendo_base(
-    call = call,
-    F.formula = F.formula,
-    mf = mf,
-    coefficients = estimated$coefficients,
-    names.main.coefs = estimated$names.main.coefs,
-    fitted.values = estimated$fitted.values,
-    residuals = estimated$residuals,
-    subclass = "rendo.copula.ima"
-  )
-}
-
 #with bootstrap
 
 build_rendo_boots_ima <- function(call, F.formula, mf, estimated, boots) {
