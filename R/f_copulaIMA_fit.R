a1b0487e02e8ab4d91625bb9e10460292fa3256c
KLewCW <kimberlylew12@gmail.com>
2026-02-05 20:35:16 +0100

    Update f_copulaIMA_fit.R
    1)Removed the '...' from the function signature.
    2) Removed rhs1.vars and rhs2.vars (variables are not used later)
    3) removed the intercept check 'has.intercept' (only included it to check for intercept, but the fit already deals with it. has.intercept is not used later)
    4) rewrote cop.terms (as this also exist in copulaIMA_residuals() )
    5) removed '.new_rendo_base()' and replaced it to return fit directly, as it is an internal object.

--- R/f_copulaIMA_fit.R
+++ R/f_copulaIMA_fit.R
@@ -1,11 +1,8 @@
 #' @importFrom Formula as.Formula
 #' @importFrom stats lm model.frame model.matrix terms qnorm
-CopulaIMA_fit <- function(F.formula, data, cdf,...){
+CopulaIMA_fit <- function(F.formula, data, cdf){
 
   F.formula <- Formula::as.Formula(F.formula)
-
-  rhs1.vars <- all.vars(formula(F.formula, rhs = 1, lhs = 0))
-  rhs2.vars <- all.vars(formula(F.formula, rhs = 2, lhs = 0))
 
   names.vars.continuous <- formula_readout_special( F.formula = F.formula,name.special = "continuous",from.rhs = 2,params.as.chars.only = TRUE)
 
@@ -15,9 +12,6 @@ CopulaIMA_fit <- function(F.formula, data, cdf,...){
   y <- model.response(mf)
 
   X.main <- model.matrix(formula(F.formula, rhs = 1, lhs = 0), mf)
-
-  #checking if there are intercepts
-  has.intercept <- attr(terms(F.formula, rhs = 1), "intercept") == 1
 
   #Continuous endogenous var
   endogenous.columns <- colnames(X.main)[colnames(X.main) %in% names.vars.continuous]
@@ -33,28 +27,13 @@ CopulaIMA_fit <- function(F.formula, data, cdf,...){
 
   #checking for exogeneous variables
 
- if (ncol(P)==1){
-   cop.terms <- qnorm(P.star)
-   colnames(cop.terms) <- paste0 (P.names, "_cop")
- } else{
-   cop.terms <- copulaIMA_residuals(P.star, P.names)
- }
-
+  cop.terms <- copulaIMA_residuals(P.star, P.names)
 
   X.final <- cbind(X.main, cop.terms)
 
   # X.main contains intercept if specified in the original formula.
   fit <- lm(y ~ X.final - 1) #intercept is removed to fit without an additional implicit intercept
 
-  .new_rendo_base(
-    F.formula = F.formula,
-    mf = mf,
-    coefficients = coef(fit),
-    names.main.coefs = colnames(X.main),
-    fitted.values = fitted(fit),
-    residuals = residuals(fit),
-    subclass = "rendo.copula.ima",
-    cdf = cdf
-  )
+  return(fit)
 }
 
