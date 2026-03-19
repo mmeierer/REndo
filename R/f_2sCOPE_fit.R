9b2c839574c93dff2067cc3549215e1a5c48e41e
KLewCW <kimberlylew12@gmail.com>
2026-03-19 20:14:12 +0100

    Update f_2sCOPE_fit.R

--- R/f_2sCOPE_fit.R
+++ R/f_2sCOPE_fit.R
@@ -8,8 +8,7 @@ copula2scope_fit <- function(F.formula, data, cdf){
 
   F.formula.main <- formula(F.formula, rhs = 1, lhs = 1)
   mf <- model.frame (F.formula.main, data = data)
-  y <- model.response(mf)
-  X.main <- model.matrix(formula(F.formula, rhs = 1, lhs = 0 ), mf)
+  X.main <- model.matrix(F.formula.main, data = mf)
 
   endogenous.columns <- colnames(X.main)[colnames(X.main) %in% names.vars.continuous]
 
@@ -19,14 +18,26 @@ copula2scope_fit <- function(F.formula, data, cdf){
   if (length(endogenous.columns) < length(names.vars.continuous)){
     stop("Bootstrap sample dropped at least one endogenous regressor. This happened when a regressor becomes constant in the resampling.")}
 
-
+  #applying CDF to all regressors both endo and exo with no intercept column
   X.no.intercept <- X.main[, colnames(X.main) != "(Intercept)", drop = FALSE]
   P.star <- copula2scope_pstar(X.no.intercept, cdf = cdf)
+
+  #first-stage residuals: regressing each endogenous P* on exogenous W*, Eq (9) from Yang et. al 2024
   cop.terms <- copula2scope_residuals(P.star, endo.cols = endogenous.columns)
 
-  X.final <- cbind(X.main, cop.terms)
+  f.main <- formula(mf)
 
-  fit <- lm(y ~ X.final - 1)
+  f.pcop <- reformulate(
+    termlabels = c(".", colnames(cop.terms)),
+    response = NULL,
+    intercept = TRUE
+  )
 
-  return(fit)
+
+  f.final <- update(old = f.main, new = f.pcop)
+
+  return(lm(
+    formula = f.final,
+    data = cbind(data, cop.terms)
+  ))
 }
